35ee5f1c079f2ed0aba6e6bbbc0192fac6de0d90
Marian <marianlopig@gmail.com>
2022-05-30 17:06:32 +0200

    initialize server and user model

---
+++ src/server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { notFoundError, generalError } = require("./middlewares/errors/errors");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use(notFoundError);
+app.use(generalError);
+
+module.exports = app;
