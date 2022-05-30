b3746099d9e57b07d46cd495155e1e617a111d3c
Marian <marianlopig@gmail.com>
2022-05-30 16:31:49 +0200

    error middlewares

---
+++ src/server/middlewares/errors/errors.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const debug = require("debug")("gamersland:server:middlewares:errors");
+const chalk = require("chalk");
+const { ValidationError } = require("express-validation");
+const customError = require("../../../utils/customError");
+
+const notFoundError = (req, res, next) => {
+  const error = customError(404, "Endpoint not found");
+
+  next(error);
+};
+
+// eslint-disable-next-line no-unused-vars
+const generalError = (error, req, res, next) => {
+  debug(chalk.red(error.message || error.customMessage));
+  let message = error.customMessage ?? "General pete";
+  const statusCode = error.statusCode ?? 500;
+
+  if (error instanceof ValidationError) {
+    message = "Wrong data";
+    debug(chalk.red(JSON.stringify(error.details)));
+  }
+  res.status(statusCode).json({ error: true, message });
+};
+
+module.exports = {
+  notFoundError,
+  generalError,
+};
