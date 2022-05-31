7fbbde75ba06238a3d993a341465788e5a9ec579
Marian <marianlopig@gmail.com>
2022-05-31 23:38:39 +0200

    register controller added

--- src/controllers/userControllers/userControllers.js
+++ src/controllers/userControllers/userControllers.js
@@ -31,4 +31,33 @@ const userLogin = async (req, res, next) => {
   res.status(200).json({ token });
 };
 
-module.exports = userLogin;
+const userRegister = async (req, res, next) => {
+  const { name, username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (user) {
+    const error = new Error();
+    error.statusCode = 409;
+    error.message = "This user already exists...";
+
+    next(error);
+    return;
+  }
+  try {
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const newUser = { name, username, password: encryptedPassword };
+
+    await User.create(newUser);
+
+    res.status(201).json({ username });
+  } catch (error) {
+    error.statusCode = 400;
+    error.message = "Wrong user data..";
+    next(error);
+  }
+};
+
+module.exports = {
+  userRegister,
+  userLogin,
+};
