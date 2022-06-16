53a19c0837aeaf3b64dcef8d14f3738c8d630987
Marian <marianlopig@gmail.com>
2022-06-16 04:03:05 +0200

    added a new firebase account

--- src/server/routers/parkRouter.js/parkRouter.js
+++ src/server/routers/parkRouter.js/parkRouter.js
@@ -14,7 +14,7 @@ const { saveImages } = require("../../middlewares/saveImages/saveImages");
 const upload = multer({
   dest: "images/",
   limits: {
-    fileSize: 10000000,
+    fileSize: 1000000,
   },
 });
 
